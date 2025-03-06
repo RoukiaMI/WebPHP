c7f97144e77a67910a06e998c31752b85eb5b2ff
RoukiaMI <78264320+RoukiaMI@users.noreply.github.com>
2025-03-06 14:53:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-date_default_timezone_set('Europe/Paris'); // Définir le fuseau horaire
-echo "Heure actuelle : AppService" . date("H:i:s");
+//date_default_timezone_set('Europe/Paris'); // Définir le fuseau horaire
+//echo "Heure actuelle : AppService" . date("H:i:s");
+echo "Bobnjfhisuqfdn";
 ?>
